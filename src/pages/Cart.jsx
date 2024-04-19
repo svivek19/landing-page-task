8bf632fa8d89b0bd9877333763733597ae911403
svivek19 <svivek030503@gmail.com>
2024-04-19 16:23:19 +0530

    feat: checkout

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -16,26 +16,35 @@ const Cart = () => {
       {cartItems.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
-        <ul className="mt-4">
-          {cartItems.map((item, index) => (
-            <li
-              key={index}
-              className="border-b py-2 flex justify-between items-center"
-            >
-              <div>
-                <h3 className="text-xl font-semibold">{item.title}</h3>
-                <p className="text-gray-600">{item.description}</p>
-                <p className="mt-2">Price: ₹{item.price}</p>
-              </div>
-              <button
-                onClick={() => handleRemove(index)}
-                className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded"
+        <div>
+          <ul className="mt-4">
+            {cartItems.map((item, index) => (
+              <li
+                key={index}
+                className="border-b py-2 flex justify-between items-center"
               >
-                Remove
-              </button>
-            </li>
-          ))}
-        </ul>
+                <div>
+                  <h3 className="text-xl font-semibold">{item.title}</h3>
+                  <p className="text-gray-600">{item.description}</p>
+                  <p className="mt-2">Price: ₹{item.price}</p>
+                </div>
+                <button
+                  onClick={() => handleRemove(index)}
+                  className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded"
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+
+          <div className="mt-8 text-xl font-semibold">
+            <h1>Total: {cartItems.map((item) => item.price)}</h1>
+            <button className="bg-slate-900 px-6 py-1 font-normal rounded text-base my-4 text-white">
+              Checkout
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
